e6fb736dc8da68ba78e4af444a15dbfccbb7139b
izhxl <jon2000q@gmail.com>
2026-02-15 14:05:17 +0100

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,13 @@
 // 🔥 Firebase config (incolla qui il firebaseConfig del tuo progetto)
 // Firebase Console → Impostazioni progetto → Le tue app (Web) → "Config"
 //
-// window.FIREBASE_CONFIG = {
-//   apiKey: "...",
-//   authDomain: "...",
-//   projectId: "...",
-//   storageBucket: "...",
-//   messagingSenderId: "...",
-//   appId: "..."
-// };
-window.FIREBASE_CONFIG = null;
+// firebase-config.js
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBZbB2YIZREKapXB3_O9Sn6QYzg1pFtiu0",
+  authDomain: "metintimer-c235f.firebaseapp.com",
+  projectId: "metintimer-c235f",
+  storageBucket: "metintimer-c235f.firebasestorage.app",
+  messagingSenderId: "161438380582",
+  appId: "1:161438380582:web:63f18b97968b3693e56ce7",
+  measurementId: "G-QE9PC4T5PL"
+};
