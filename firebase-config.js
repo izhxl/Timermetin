d8da72bbcd3118e14415e267abeb2b7f15b776e4
izhxl <jon2000q@gmail.com>
2026-02-15 19:33:06 +0100

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-window.FIREBASE_CONFIG = {
-  apiKey: "AIzaSyBZbB2YIZREKapXB3_O9Sn6QYzg1pFtiu0",
-  authDomain: "metintimer-c235f.firebaseapp.com",
-  projectId: "metintimer-c235f",
-  storageBucket: "metintimer-c235f.firebasestorage.app",
-  messagingSenderId: "161438380582",
-  appId: "1:161438380582:web:63f18b97968b3693e56ce7",
-  measurementId: "G-QE9PC4T5PL"
-};
